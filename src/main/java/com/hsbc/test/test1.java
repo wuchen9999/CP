8ae9ac6b6ee4f503b98b3cd13e41dcc0c421c487
wuchen9999 <453821963@qq.com>
2019-01-20 23:36:30 +0800

    create project.

---
+++ src/main/java/com/hsbc/test/test1.java
@@ -0,0 +1,12 @@
+package com.hsbc.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class test1 {
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(test1.class);
+		logger.info("Hello World");
+	}
+ }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<body>
+<h2>Hello World!d大师傅但是</h2>
+</body>
+</html>
